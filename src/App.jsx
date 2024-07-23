6d718430d367e9e186070b332cbe19ec79d3e6a3
anwarbakkar <anwarbkkr007@gmail.com>
2024-07-23 15:09:21 -0700

    add Error Message

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import Navbar from './component/Layout/Navbar'
 import User from './component/User/User';
 import Search from './component/User/Search';
 import "./App.css";
+import Alert from './component/User/Alert';
 
 const App = () => {
   const [user,setUser] = useState([]);
+  const [error,setError] = useState([]);
   // const fetchData = async () => {
   //   const url = await axios.get("https://api.github.com/users");
   //   setUser(url.data);
   // }
   
   const onSearchTextHandler = async (text) => {
+    setError(text);
+    if(!text == ""){
+      const url = (`https://api.github.com/search/users?q=${text}&client_id=${import.meta.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${import.meta.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+      const res = await axios.get(url);
+      setUser(res.data.items);
+      console.log("hello",res.data.items)
+    }
 
-    const url = (`https://api.github.com/search/users?q=${text}&client_id=${import.meta.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${import.meta.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    const res = await axios.get(url);
-    setUser(res.data.items);
-    console.log("hello",res.data.items)
   }
   useEffect(() => {
     onSearchTextHandler();
@@ -30,6 +35,8 @@ const App = () => {
   return (
     <>
       <Navbar></Navbar>
+     {!error && <Alert></Alert>}
+      {/* <Alert></Alert> */}
       <Search onSearchText={onSearchTextHandler} onClearUsers={clearUserHandler} showUsers={user.length > 0 ? true:false}></Search>
       <User user={user}></User>
     </>
---
+++ src/component/User/Alert.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Alert.css'
+
+const Alert = () => {
+  return (
+    <div className='ErrorMessage'>
+        <h4><i className="fa-solid fa-exclamation"></i>please Input UserName </h4>
+    </div>
+  )
+}
+
+export default Alert
